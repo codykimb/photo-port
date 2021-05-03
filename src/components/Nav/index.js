9c2319db1cd38e3cab1de3a27c4c9aeb99999ecc
Cody Kimb <codykimbell@gmail.com>
2021-05-03 11:48:32 -0500

    contact form and tests

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,11 +2,13 @@ import React, { useEffect } from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
-  const {
-    categories = [],
-    setCurrentCategory,
-    currentCategory,
-  } = props;
+    const {
+        categories = [],
+        setCurrentCategory,
+        currentCategory,
+        contactSelected,
+        setContactSelected
+      } = props;
 
   useEffect(() => {
     document.title = capitalizeFirstLetter(currentCategory.name);
@@ -21,26 +23,26 @@ function Nav(props) {
       </h2>
       <nav>
         <ul className="flex-row">
-          <li className="mx-2">
-            <a data-testid="about" href="#about">
+        <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
               About me
             </a>
           </li>
           <li className="mx-2">
-            <span>Contact</span>
+          <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
             <li
-              className={`mx-1 ${
-                currentCategory.name === category.name && 'navActive'
+                className={`mx-1 ${
+                currentCategory.name === category.name && !contactSelected && `navActive`
                 }`}
-              key={category.name}
+                key={category.name}
             >
-              <span
-                onClick={() => {
-                  setCurrentCategory(category)
+              <span onClick={() => {
+                    setCurrentCategory(category);
+                    setContactSelected(false);
                 }}
-              >
+                >
                 {capitalizeFirstLetter(category.name)}
               </span>
             </li>
